e1ddaf2f329c1b3ad368fa6a4a9e53aca3fa55cf
nyanlinaungn219 <nyanlinaungn219@gmail.com>
2026-06-24 12:28:30 +0630

    Schedule Run with Dynamic

--- run_batch.bat
+++ run_batch.bat
@@ -1,3 +1,13 @@
 @echo off
 cd /d "%~dp0"
+
+if /I "%1"=="runjob" goto RUNJOB
+
+schtasks /delete /tn "BatchTask" /f >nul 2>&1
+schtasks /create /tn "BatchTask" /tr "\"%~dp0run_batch.bat\" runjob" /sc minute /mo 3 /f
+goto END
+
+:RUNJOB
 bin\BATCH-UPDATE.EXE >> batch_log.txt 2>&1
+
+:END
